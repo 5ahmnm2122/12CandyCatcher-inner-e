3d5f4f992eb1d56e32e8d298e8dac62c6ed5ff44
Eva Innerlohninger <evainnerlohninger@Eva-M1.local>
2022-01-22 15:43:03 +0100

    add stats manager, not working yet

--- Assets/MyGame/scripts/StatsManager.cs
+++ Assets/MyGame/scripts/StatsManager.cs
@@ -7,13 +7,34 @@ public class StatsManager : MonoBehaviour
 {
     public string userName;
     public int score;
+    public Text playersName;
+    public InputField nameInput;
+    public static string nameOfPlayer;
+    public Text scoreText;
+    public static int scoreUpdate = 0;
 
-    // Start is called before the first frame update
-    void RemeberName()
+
+    private void Update()
     {
+       playersName.text = nameOfPlayer;
+       
+    }
+
+
+    public void Reset()
+    {
+
+        var InputField = nameInput.GetComponent<InputField>();
+        InputField.text = playersName.text;
+
+    }
+
+    /* void RemeberName()
+    {
+       
         GameObject.Find("buttonStart").GetComponent<Button>().interactable =
             GameObject.Find("userName").GetComponent<InputField>().text.Length >= 4 ? true : false;
         userName = GameObject.Find("userName").GetComponent<InputField>().text;
     }
-
+     */
 }
